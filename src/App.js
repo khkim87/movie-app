64a2eacd956d255cfb1f469385a453f7b9c029d0
khkim <wayre@naver.com>
2021-06-12 22:44:34 +0900

    init Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state ={
+  }
+
+  componentDidMount(){
+    this._getMovies();
+    
+  }
+
+   _getMovies = async () =>{
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  _renderMovies = () =>{
+    const movies = this.state.movies.map( movie => 
+      <Movie 
+      title={movie.title} 
+      poster={movie.medium_cover_image} 
+      key={movie.id} 
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      />
+    );
+
+    return movies;
+  }
+  
+  render(){
+    const {movies} = this.state;
+    return (
+      <div className={movies?"App":"App--loading"}>
+        {
+          movies ? this._renderMovies() : 'Loading'
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
